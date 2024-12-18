b8ac2aa152101e0a5c267a6db08983d91eb6d670
Alex Shepard <alex@meatfreezer.com>
2024-12-18 11:18:33 -0800

    initial xcode / swiftui project

---
+++ EverydayShaders/EverydayShadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverydayShadersApp.swift
+//  EverydayShaders
+//
+//  Created by Alex Shepard on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EverydayShadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
