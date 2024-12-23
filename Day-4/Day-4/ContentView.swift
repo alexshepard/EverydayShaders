fa978ab05509ff6d8f40e5e85243d3b1528c79de
Alex Shepard <alex@meatfreezer.com>
2024-12-23 13:11:09 -0800

    stating point for day 4
    basically where we were yesterday with day 3

---
+++ Day-4/Day-4/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Day-4
+//
+//  Created by Alex Shepard on 12/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MetalView()
+            Text("Day 4 - Cycling colors")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day-4/Day-4/Day_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_4App.swift
+//  Day-4
+//
+//  Created by Alex Shepard on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
